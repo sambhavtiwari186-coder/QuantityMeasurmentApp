2f4072bfba0d75c395f1cd322f4bfdc7af0a337e
Anuj Verma <sambhavtiwari71@gmail.com>
2026-03-10 11:29:03 +0530

    [Sambhav Tiwari] Implemented Genric Length in the Project

--- QuantityMeasurement.App/Program.cs
+++ QuantityMeasurement.App/Program.cs
@@ -10,43 +10,33 @@ namespace QuantityMeasurement.App
         {
             QuantityMeasurementService service = new QuantityMeasurementService();
 
-            Console.WriteLine("1. Compare Feet");
-            Console.WriteLine("2. Compare Inch");
-            Console.Write("Select option: ");
+           Console.WriteLine("1. Compare Feet");
+Console.WriteLine("2. Compare Inch");
+Console.WriteLine("3. Compare Feet & Inch");
+Console.Write("Select option: ");
 
-            int choice = Convert.ToInt32(Console.ReadLine());
+int choice = Convert.ToInt32(Console.ReadLine());
 
-            try
-            {
-                Console.Write("Enter first value: ");
-                double value1 = Convert.ToDouble(Console.ReadLine());
+try
+{
+    if (choice == 3)
+    {
+        Console.Write("Enter value in Feet: ");
+        double feetValue = Convert.ToDouble(Console.ReadLine());
 
-                Console.Write("Enter second value: ");
-                double value2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter value in Inch: ");
+        double inchValue = Convert.ToDouble(Console.ReadLine());
 
-                if (choice == 1)
-                {
-                    Feet f1 = new Feet(value1);
-                    Feet f2 = new Feet(value2);
+        Feet feet = new Feet(feetValue);
+        Inch inch = new Inch(inchValue);
 
-                    Console.WriteLine($"Equal? {service.CompareFeet(f1, f2)}");
-                }
-                else if (choice == 2)
-                {
-                    Inch i1 = new Inch(value1);
-                    Inch i2 = new Inch(value2);
-
-                    Console.WriteLine($"Equal? {service.CompareInch(i1, i2)}");
-                }
-                else
-                {
-                    Console.WriteLine("Invalid option.");
-                }
-            }
-            catch
-            {
-                Console.WriteLine("Invalid numeric input.");
-            }
+        Console.WriteLine($"Equal? {service.CompareFeetAndInch(feet, inch)}");
+    }
+}
+catch
+{
+    Console.WriteLine("Invalid input.");
+}
         }
     }
 }
--- QuantityMeasurement.Library/Service/QuantityMeasurmentService.cs
+++ QuantityMeasurement.Library/Service/QuantityMeasurmentService.cs
@@ -5,6 +5,8 @@ namespace QuantityMeasurement.Library.Service
 {
     public class QuantityMeasurementService
     {
+        private const double FEET_TO_INCH = 12.0;
+
         public bool CompareFeet(Feet first, Feet second)
         {
             if (first == null || second == null)
@@ -19,6 +21,16 @@ namespace QuantityMeasurement.Library.Service
                 throw new ArgumentNullException("Inch values cannot be null");
 
             return first.Equals(second);
+        }
+
+        public bool CompareFeetAndInch(Feet feet, Inch inch)
+        {
+            if (feet == null || inch == null)
+                throw new ArgumentNullException("Values cannot be null");
+
+            double feetInInches = feet.Value * FEET_TO_INCH;
+
+            return feetInInches.CompareTo(inch.Value) == 0;
         }
     }
 }
